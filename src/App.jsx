d0cbc04e719376e88d0732e7667f91ec39021387
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-02-03 15:49:54 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+
+export default function App(){
+  return (
+      <Router>
+        <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+      </Router>
+  )
+}
